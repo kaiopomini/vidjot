c8c0db975be92219df73f7275e9ea7b699843d72
Kaio Pomini <kaiopomini@gmail.com>
2020-07-06 00:32:34 -0300

    CRUD Operators

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const Handlebars = require('handlebars');
+const methodOverride = require('method-override');
+const { request } = require('https');
 
 const app = express();
 
@@ -20,8 +25,17 @@ mongoose.connect('mongodb://localhost/vidjot-dev',
 .catch(err => console.log(err));
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Method override middleware
+app.use(methodOverride('_method'));
 
 // Index Route
 app.get('/', (req, res) => {
@@ -35,6 +49,88 @@ app.get('/', (req, res) => {
 // About page
 app.get('/about', (req, res) =>{
   res.render('about');
+});
+
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas:ideas
+      });
+    });
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) =>{
+  res.render('ideas/add');
+});
+
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) =>{
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    res.render('ideas/edit', {
+      idea: idea
+    })
+  });
+});
+
+// Process Form
+app.post('/ideas', (req, res) =>{
+  let errors = [];
+  if(!req.body.title) {
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details) {
+    errors.push({text:'Please add some details'});
+  }
+
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      });
+  }
+});
+
+// Edit form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    // new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    
+    idea.save()
+      .then(idea =>{
+        res.redirect('/ideas');
+      });
+  });
+});
+
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+  Idea.remove({_id: req.params.id})
+    .then(() => {
+      res.redirect('/ideas');
+    });
 });
 
 const port = 5000;
